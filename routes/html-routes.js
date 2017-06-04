f3d36f646516526a8b1eaa4f3925590c8209fe36
Melissa Morgan <melissamorgan@Melissas-MacBook-Pro.local>
2017-06-03 22:10:53 -0400

    modified changes

--- routes/html-routes.js
+++
@@ -1,8 +0,0 @@
-// var express = require('express');
-// var path = require("path");
-
-//   router.get("/", function(req, res) {
-//     res.sendFile(path.join(__dirname, "/../public/login.html"));
-//   });
-
-// module.exports = router;
